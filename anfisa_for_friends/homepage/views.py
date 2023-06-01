267acf9130958010b9db80972b32385cef86502f
Константин Селиков <selikow@hotmail.com>
2023-06-01 22:45:21 +0300

    final

---
+++ anfisa_for_friends/homepage/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+from ice_cream.models import IceCream
+
+def index(request):
+    return render(request, 'homepage/index.html',
+    {'ice_cream_list': IceCream.objects.values(
+    'id','title', 'price', 'description').filter(
+    is_published=True,
+    is_on_main=True,
+    category__is_published=True)})
